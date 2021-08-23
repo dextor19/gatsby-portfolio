462c060413a23ff13ff3897a68c1bcc65340d0a2
Dexter Griffiths <dexgriffiths@hotmail.co.uk>
2021-08-24 00:56:13 +0900

    added button to scroll to top of page

--- dexter-portfolio/src/components/Layout.js
+++ dexter-portfolio/src/components/Layout.js
@@ -6,6 +6,23 @@ import Footer from "./Footer"
 import Navbar from "./Navbar"
 import { GlobalStyle } from "./globalStyle"
 import { MainWrapper } from "./layoutComponents"
+import { AnchorLink } from "gatsby-plugin-anchor-links";
+import styled from "styled-components"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
+
+const IconContainer = styled.div`
+  position: fixed;
+  text-align: center;
+  bottom: 12px;
+  right: 16px;
+  background-color: #00000073;
+  border-radius: 30px;
+  width: 30px;
+  height: 30px;
+  padding-top: 3px;
+  
+`;
 
 const Layout = ({ children, padding, fixedNavbar}) => {
       return (
@@ -16,14 +33,19 @@ const Layout = ({ children, padding, fixedNavbar}) => {
           </Helmet>
           {fixedNavbar ? <Navbar fixedNav /> : <Navbar /> }
             {padding ? (
-                <MainWrapper padded>
+                <MainWrapper padded id="to-top">
                     <main>{children}</main>
                 </MainWrapper>
             ) : ( 
-                <MainWrapper>
+                <MainWrapper id="to-top">
                     <main>{children}</main>
                 </MainWrapper>
             )}
+          <AnchorLink to="#to-top">
+            <IconContainer>
+              <FontAwesomeIcon icon={ faAngleUp } size="lg" color="white" /> 
+            </IconContainer>
+          </AnchorLink>
           <Footer />
         </>
     )

0d880e41c6a89ab604e1acb541430ab18124735d
Dexter Griffiths <dexgriffiths@hotmail.co.uk>
2022-03-29 10:39:33 +0900

    add thank you page for netlify messages

---
+++ src/pages/thanks.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+
+const StyledSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #f7f7f7;
+  min-height: 85vh;
+  @media screen and (max-width: 768px) {
+    min-height: 86.7vh;
+  }
+`;
+
+const ThanksPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+      <StyledSection>
+          <h1>Thank you</h1>
+          <p>Thank you for the message, I will get back to you soon.</p>
+      </StyledSection>
+  </Layout>
+);
+
+export default ThanksPage;
